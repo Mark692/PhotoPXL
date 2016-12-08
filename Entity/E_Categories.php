91f57f98a4dec3263b29c332eb81526816ccf250
Marco Carolla <chiocciolalibero.eu@libero.it>
2016-12-08 16:33:34 +0100

    Entity, DONE. Revision 2: Categories modified

--- Entity/E_Categories.php
+++ Entity/E_Categories.php
@@ -11,18 +11,75 @@ namespace Entity;
 class Categories {
     
     /**
-     * Categories to describe both photos and albums
-     */ 
-        const PAESAGGI = 0;
-        const RITRATTI = 1;
-        const FAUNA = 2;
-        const MACRO = 3;
-        const BIANCO_NERO = 4;
-        const ASTRONOMIA = 5;
-        const STREET = 6;
-        const NATURA_MORTA = 7;
-        const SPORT = 8;
+     * Categories array that describes both photos and albums
+     *
+    const PAESAGGI;
+    const RITRATTI;
+    const FAUNA;
+    const MACRO;
+    const BIANCO_NERO;
+    const ASTRONOMIA;
+    const STREET;
+    const NATURA_MORTA;
+    const SPORT;
+    */
+    private $categories;
     
+    
+    /**
+     * Instantiate an array of categories 
+     * @param array $cat
+     */
+    public function __construct(array $cat) {
+        $this->categories = $cat;
+    }
+    
+    /**
+     * Changes radically the previous instantiated array of categories
+     * @param array $new_cats
+     */
+    public function set(array $new_cats) {
+        $this->categories = $new_cats;
+    }
+    
+    
+    /**
+     * Retrives the categories array
+     * @return array
+     */
+    public function get() {
+        return $this->categories;
+    }
+    
+    
+    /**
+     * Adds the string/array of categories to $this->categories
+     * @param string $s_del
+     * @param array $a_del
+     */
+    public function add(string $s_del="", array $a_del=[]) {
+        if($s_del!="") {
+            array_push($this->categories, $s_del);
+        }
+        if($a_del!=[]) {
+            array_push($this->categories, $a_del);
+        }
+    }
+    
+    
+    /**
+     * Removes the string/array of categories from $this->categories
+     * @param string $s_del
+     * @param array $a_del
+     */
+    public function remove(string $s_del="", array $a_del=[]) {
+        if($s_del!="") {
+            array_diff($this->categories, $s_del);
+        }
+        if($a_del!=[]) {
+            array_diff($this->categories, $a_del);
+        }
+    }
     
     
     
