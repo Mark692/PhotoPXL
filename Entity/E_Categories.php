77943037396d68f50bb4d638730207a88edfcfc0
Marco Carolla <chiocciolalibero.eu@libero.it>
2016-12-09 15:29:31 +0100

    Prova Commit

--- Entity/E_Categories.php
+++ Entity/E_Categories.php
@@ -1,6 +1,6 @@
 <?php
 
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -9,7 +9,7 @@
 namespace Entity;
 
 class Categories {
-    
+
     /**
      * Categories array that describes both photos and albums
      *
@@ -24,17 +24,17 @@ class Categories {
      * SPORT
      */
     private $categories;
-    
-    
+
+    //I TRE PORCELLINI
+
     /**
-     * Instantiate an array of categories 
+     * Instantiate an array of categories
      * @param array $cat
      */
     public function __construct(array $cat) {
         $this->categories = $cat;
     }
-    
-    
+
     /**
      * Changes radically the previous instantiated array of categories
      * @param array $new_cats
@@ -42,8 +42,7 @@ class Categories {
     public function set(array $new_cats) {
         $this->categories = $new_cats;
     }
-    
-    
+
     /**
      * Retrives the categories array
      * @return array
@@ -51,31 +50,27 @@ class Categories {
     public function get() {
         return $this->categories;
     }
-    
-    
+
     //--------------NOT--REALLY--USED--METHODS-------------------------
-    
+
     /**
      * Adds the string/array of categories to $this->categories
      * @param string or array $to_add
      */
     public function add($to_add) {
-        if($to_add!="" && $to_add!=[]) {
+        if ($to_add != "" && $to_add != []) {
             array_push($this->categories, $to_add);
         }
     }
-    
-    
+
     /**
      * Removes the string/array of categories from $this->categories
      * @param string or array $to_del
      */
     public function remove($to_del) {
-        if($to_del!="" && $to_del!=[]) {
+        if ($to_del != "" && $to_del != []) {
             array_diff($this->categories, $to_del);
         }
     }
-    
-    
-    
+
 }
