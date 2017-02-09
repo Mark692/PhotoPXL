e97a22edb8119138ed01728414c3b2aa6d8cdac5
Marco Carolla <chiocciolalibero.eu@libero.it>
2017-02-09 12:16:06 +0100

    Try in E_User Catch in C_Registrazione

--- Prove/Control/TC_Registrazione.php
+++ Prove/Control/TC_Registrazione.php
@@ -12,7 +12,24 @@ class TC_Registrazione extends \Prove\TFun
 {
 
 
+    public function save_User()
+    {
+        $username = parent::rnd_str(10)."£";
+        $password = parent::rnd_str(10);
+        $email = parent::rnd_str(10);
+
+        try
+        {
+            $e_userSTD = new \Entity\E_User_Standard($username, $password, $email);
+            \Foundation\F_User::insert($e_userSTD);
+        }
+        catch (Exceptions\InvalidInput $ex)
+        {
+            echo($ex); //LANCIA TEMPLATE PER GESTIRE ERRORE
+        }
+        //RITORNA IL TEMPLATE ALLA REGISTRAZIONE
+    }
 
 
-    
+
 }
