6298cab1b6dba2e2d76a424a8f2e2369807ccbef
Marco Carolla <chiocciolalibero.eu@libero.it>
2017-02-11 18:54:21 +0100

    COSE 3.0

--- Entity/E_Photo.php
+++ Entity/E_Photo.php
@@ -20,13 +20,8 @@ class E_Photo
     private $is_reserved;
     private $upload_date;
     private $categories = []; //Categorie della foto
-    private $likes;
-    private $comments = [];
-
-    private $fullsize;
-    private $thumbnail;
-    private $size;
-    private $type;
+    private $total_likes;
+    private $total_comments;
 
 
     /**
@@ -199,7 +194,7 @@ class E_Photo
      */
     public function set_Likes($user_list)
     {
-        $this->likes = $user_list;
+        $this->total_likes = $user_list;
     }
 
 
@@ -210,7 +205,7 @@ class E_Photo
      */
     public function get_Likes()
     {
-        return $this->likes;
+        return $this->total_likes;
     }
 
 
@@ -221,7 +216,7 @@ class E_Photo
      */
     public function get_Total_Likes()
     {
-        return count($this->likes);
+        return count($this->total_likes);
     }
 
 
@@ -232,7 +227,7 @@ class E_Photo
      */
     public function add_Like($username)
     {
-        array_push($this->likes, $username);
+        array_push($this->total_likes, $username);
     }
 
 
@@ -243,12 +238,12 @@ class E_Photo
      */
     public function remove_Like($username)
     {
-        $user_key = array_search($username, $this->likes);
+        $user_key = array_search($username, $this->total_likes);
             if ($user_key !== FALSE) //Exists in the "likes" array
             {
                 unset($this->categories[$user_key]);
             }
-        $this->likes = array_values($this->likes); //Ordinates the array without any gaps in between the keys
+        $this->total_likes = array_values($this->total_likes); //Ordinates the array without any gaps in between the keys
     }
 
 
@@ -275,159 +270,23 @@ class E_Photo
 
 
     /**
-     * Sets a list of comments for this photo
+     * Sets the number of comments for this photo
      *
-     * @param array $comments The list of comments for this photo
+     * @param int $comments The number of comments for this photo
      */
     public function set_Comments($comments)
     {
-        $this->comments = $comments;
+        $this->total_comments = $comments;
     }
 
 
     /**
-     * Retrieves the list of comments made for this photo
+     * Retrieves the number of comments for this photo
      *
-     * @return array The list of comments for this photo
+     * @return array The number of comments for this photo
      */
     public function get_Comments()
     {
-        return $this->comments;
-    }
-
-
-    /**
-     * Adds a comment at the current photo
-     *
-     * @param string $comment The comment to add
-     */
-    public function add_Comment($comment)
-    {
-        array_push($this->comments, $comment);
-    }
-
-
-    /**
-     * Removes a comment if exists
-     *
-     * @param int $comment_ID The comment's ID to remove
-     */
-    public function remove_Comment($comment_ID)
-    {
-        if(in_array($comment_ID, $this->comments))
-        {
-            unset($this->comments[$comment_ID]);
-        }
-        $this->categories = array_values($this->categories); //Ordinates the array without any gaps in between the keys
-    }
-
-
-    /**
-     * Sets the fullsize image from the path given as parameter
-     *
-     * @param string $path The path to the photo
-     * @throws \Exceptions\photo_details Whether the path to the photo is incorrect
-     */
-    public function set_Fullsize($path)
-    {
-        if(realpath($path)===FALSE)
-        {
-            throw new \Exceptions\photo_details(0, $path);
-        }
-        $this->fullsize = realpath($path);
-    }
-
-
-    /**
-     * Returns the fullsize photo
-     *
-     * @return image The fullsize photo
-     */
-    public function get_Fullsize()
-    {
-        return $this->fullsize;
-    }
-
-
-    /**
-     * Creates and sets a thumbnail image
-     *
-     * @param string $path The path to the photo
-     * @throws \Exceptions\photo_details Whether the path is incorrect
-     */
-    public function set_Thumbnail($path)
-    {
-        if(realpath($path)===FALSE)
-        {
-            throw new \Exceptions\photo_details(1, $path);
-        }
-        $imagick = new \Imagick(realpath($path));
-        $width = THUMBNAIL_WIDTH;
-        $height = THUMBNAIL_HEIGHT;
-        $best_Fit = TRUE;
-        $fill = TRUE;
-        $imagick->thumbnailImage($width, $height, $best_Fit, $fill);
-
-        $this->thumbnail = $path;
-    }
-
-
-    /**
-     * Retrieves the thumbnail created for the photo
-     *
-     * @return image The thumbnail of the photo
-     */
-    public function get_Thumbnail()
-    {
-        return $this->thumbnail;
-    }
-
-
-    /**
-     * Sets the size of the photo
-     *
-     * @param int $size The size of the photo
-     * @throws \Exceptions\photo_details Whether the size is over the maximum allowed
-     */
-    public function set_Size($size)
-    {
-        if($size>MAX_SIZE)
-        {
-            throw new \Exceptions\photo_details(2, $size);
-        }
-        $this->size = $size;
-    }
-
-
-    /**
-     * Retrieves the size of the photo
-     *
-     * @return int The size of the photo
-     */
-    public function get_Size()
-    {
-        return $this->size;
-    }
-
-
-    /**
-     * Sets the type of the photo
-     *
-     * @param string $type The type of the photo
-     */
-    public function set_Type($type)
-    {
-        $this->type = $type;
-    }
-
-
-    /**
-     * Retrieves the type of the photo. To be used to show the correct HTML Header
-     *
-     * @return string The type of the photo
-     */
-    public function get_Type()
-    {
-        return $this->type;
+        return $this->total_comments;
     }
 }
