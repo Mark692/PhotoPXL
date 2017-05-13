66dd2efab53aaf2da2bd4ae7043e2842e3443c85
Benedetta Fabrizi <Benedetta@DESKTOP-1JA13LJ.lan>
2017-05-13 02:02:27 +0200

    administration in service

---
+++ Service/administration.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use Control\C_Administration;
+
+$username = filter_input(INPUT_POST, "username");
+$action = filter_input(INPUT_POST, "action");
+switch ($action) {
+    case "changeRole": $role = filter_input(INPUT_POST, "role");
+        $returns = C_Administration::changeRole($username, $role);
+        break;
+    case "changeRole": $returns = C_Administration::ban($username);
+        break;
+    default : $return = false;
+}
+
+echo json_encode($returns);
